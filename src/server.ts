413f8508b28af1b9810b55c447f26da49b8bea2e
Joao vitor santos <jvitor.spaula@gmail.com>
2022-08-01 18:16:59 -0300

    feat: initial setup

---
+++ src/server.ts
@@ -0,0 +1,11 @@
+import express from 'express';
+import dotenv from 'dotenv';
+dotenv.config()
+
+const app = express();
+const port = process.env.PORT || 3000;
+app.use(express.json())
+
+app.listen(port, () => {
+  console.log(`server is running in port ${port}`)
+});
